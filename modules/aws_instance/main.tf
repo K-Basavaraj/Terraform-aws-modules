c07d048c364627a6e039a5046519d94b55557dc3
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-02-26 15:09:55 +0530

    update

--- modules/aws_instance/main.tf
+++ modules/aws_instance/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "ec2_instance" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  monitoring             = var.monitoring
-  vpc_security_group_ids = var.vpc_security_group_ids
-  subnet_id              = var.subnet_id
-  tags                   = var.tags
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  monitoring                  = var.monitoring
+  vpc_security_group_ids      = var.vpc_security_group_ids
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = var.associate_public_ip_address
+  tags                        = var.tags
 }
--- modules/aws_instance/variables.tf
+++ modules/aws_instance/variables.tf
@@ -35,3 +35,8 @@ variable "tags" {
   description = "Tags to assign to the instance"
   type        = map(string)
 }
+
+variable "associate_public_ip_address" {
+  type    = bool
+  default = false
+}
