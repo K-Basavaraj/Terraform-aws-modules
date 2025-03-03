1cf7a80e57ccfbc82bf6d5286c811f01fe12ecc5
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-03-03 14:22:36 +0530

    update userdata argument

--- modules/aws_instance/main.tf
+++ modules/aws_instance/main.tf
@@ -6,5 +6,6 @@ resource "aws_instance" "ec2_instance" {
   vpc_security_group_ids      = var.vpc_security_group_ids
   subnet_id                   = var.subnet_id
   associate_public_ip_address = var.associate_public_ip_address
+  user_data                   = var.user_data
   tags                        = var.tags
 }
